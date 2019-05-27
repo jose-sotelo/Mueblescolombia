4b62ac7955c5be028d945170fa0f11b42cf14741
jose-sotelo <jsotelob1330@universidadean.edu.co>
2019-05-27 10:56:52 -0500

    Se crea la animación del menú con JavaScrip, se agregan estilos en CSS, y se enlaza con el Scrip en la parte final de HTML.

---
+++ js/menu.js
@@ -0,0 +1,6 @@
+let btnMenu = document.getElementById('btnmenu');
+let menu = document.getElementById('menu');
+btnMenu.addEventListener('click', function(){
+    'use stiric';
+    menu.classList.toggle('mostrar');
+});
